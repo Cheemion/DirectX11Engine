886f09e70146e1a1f449fbc055b122d92112c72c
kim <altroman@yeah.net>
2022-11-12 23:36:10 +0800

    T14

--- DirectX11Engine/Graphics/Graphics.h
+++ DirectX11Engine/Graphics/Graphics.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "AdapterReader.h"
 #include "Shaders.h"
+#include "Vertex.h"
+
 class Graphics
 {
 public:
@@ -9,6 +11,7 @@ public:
 private:
 	bool InitializeDirectX(HWND hwnd, int width, int height);
 	bool InitializeShaders();
+	bool InitializeScene();
 
 	Microsoft::WRL::ComPtr<ID3D11Device> device;
 	Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext;
@@ -16,4 +19,7 @@ private:
 	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView;
 	VertexShader vertexshader;
 	PixelShader  pixelshader;
+
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+
 };
